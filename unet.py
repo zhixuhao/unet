cdff6c6633f7f21b4d56787ac3fadb8c4ff76aa5
zhixuhao <zhixuhao@163.com>
2017-04-06 10:22:37 +0800

    bug fix

--- unet.py
+++ unet.py
@@ -149,31 +149,31 @@ class myUnet(object):
 		#https://github.com/orobix/retina-unet/blob/master/src/retinaNN_training.py
 
 		inputs = Input((self.img_rows, self.img_cols,1))
-		conv1 = Convolution2D(32, 3, activation='relu', border_mode='same')(inputs)
+		conv1 = Conv2D(32, 3, activation='relu', border_mode='same')(inputs)
 		conv1 = Dropout(0.2)(conv1)
-		conv1 = Convolution2D(32, 3, activation='relu', border_mode='same')(conv1)
+		conv1 = Conv2D(32, 3, activation='relu', border_mode='same')(conv1)
 		pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 		#
-		conv2 = Convolution2D(64, 3, activation='relu', border_mode='same')(pool1)
+		conv2 = Conv2D(64, 3, activation='relu', border_mode='same')(pool1)
 		conv2 = Dropout(0.2)(conv2)
-		conv2 = Convolution2D(64, 3, activation='relu', border_mode='same')(conv2)
+		conv2 = Conv2D(64, 3, activation='relu', border_mode='same')(conv2)
 		pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 		#
-		conv3 = Convolution2D(128, 3, activation='relu', border_mode='same')(pool2)
+		conv3 = Conv2D(128, 3, activation='relu', border_mode='same')(pool2)
 		conv3 = Dropout(0.2)(conv3)
-		conv3 = Convolution2D(128, 3, activation='relu', border_mode='same')(conv3)
+		conv3 = Conv2D(128, 3, activation='relu', border_mode='same')(conv3)
 
 		up1 = merge([UpSampling2D(size=(2, 2))(conv3), conv2], mode='concat', concat_axis=1)
-		conv4 = Convolution2D(64, 3, activation='relu', border_mode='same')(up1)
+		conv4 = Conv2D(64, 3, activation='relu', border_mode='same')(up1)
 		conv4 = Dropout(0.2)(conv4)
-		conv4 = Convolution2D(64, 3, activation='relu', border_mode='same')(conv4)
+		conv4 = Conv2D(64, 3, activation='relu', border_mode='same')(conv4)
 		#
 		up2 = merge([UpSampling2D(size=(2, 2))(conv4), conv1], mode='concat', concat_axis=1)
-		conv5 = Convolution2D(32, 3, activation='relu', border_mode='same')(up2)
+		conv5 = Conv2D(32, 3, activation='relu', border_mode='same')(up2)
 		conv5 = Dropout(0.2)(conv5)
-		conv5 = Convolution2D(32, 3, activation='relu', border_mode='same')(conv5)
+		conv5 = Conv2D(32, 3, activation='relu', border_mode='same')(conv5)
 		#
-		conv6 = Convolution2D(2, 1, activation='relu',border_mode='same')(conv5)
+		conv6 = Conv2D(2, 1, activation='relu',border_mode='same')(conv5)
 		conv6 = core.Reshape((2,self.img_rows*self.img_cols))(conv6)
 		conv6 = core.Permute((2,1))(conv6)
 		############
