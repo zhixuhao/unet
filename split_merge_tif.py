65de9317cd9f04fbf2b4836bb9b41d6cfb17f746
Jirka <jirka.borovec@seznam.cz>
2018-01-13 23:46:25 +0100

    fix path / dirs issue

--- split_merge_tif.py
+++ split_merge_tif.py
@@ -7,36 +7,36 @@ dirtype = ("train","label","test")
 
 def split_img():
 
-	'''
-	split a tif volume into single tif
-	'''
+    '''
+    split a tif volume into single tif
+    '''
 
-	for t in dirtype:
-		imgdir = TIFF3D.open(t + "-volume.tif")
-		imgarr = imgdir.read_image()
-		for i in range(imgarr.shape[0]):
-			imgname = t + "/" + str(i) + ".tif"
-			img = TIFF.open(imgname,'w')
-			img.write_image(imgarr[i])
+    for t in dirtype:
+        imgdir = TIFF3D.open(t + "-volume.tif")
+        imgarr = imgdir.read_image()
+        for i in range(imgarr.shape[0]):
+            imgname = t + "/" + str(i) + ".tif"
+            img = TIFF.open(imgname,'w')
+            img.write_image(imgarr[i])
 
 
 def merge_img():
-	
-	'''
-	merge single tif into a tif volume
-	'''
+    
+    '''
+    merge single tif into a tif volume
+    '''
 
-	path = '/home/zhixuhao/Downloads/imgs_mask_test_server2/'
-	imgdir = TIFF3D.open("test_mask_volume_server2.tif",'w')
-	imgarr = []
-	for i in range(30):
-		img = TIFF.open(path + str(i) + ".tif")
-		imgarr.append(img.read_image())
-	imgdir.write_image(imgarr)
+    path = '/home/zhixuhao/Downloads/imgs_mask_test_server2/'
+    imgdir = TIFF3D.open("test_mask_volume_server2.tif",'w')
+    imgarr = []
+    for i in range(30):
+        img = TIFF.open(path + str(i) + ".tif")
+        imgarr.append(img.read_image())
+    imgdir.write_image(imgarr)
 
 
 if __name__ == "__main__":
-	merge_img()
+    merge_img()
 
 
 
