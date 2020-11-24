de3b3aae2a20584af9d9fb616a604b0bb03c8e5f
Thomas Deegan <tadeegan@gmail.com>
2020-11-23 17:53:36 -0800

    Remove duplicate numpy import

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import numpy as np
 import os
 import skimage.io as io
 import skimage.transform as trans
-import numpy as np
 from keras.models import *
 from keras.layers import *
 from keras.optimizers import *
