2a3460035c94f6238fc8ece516ddf3aefa38990b
SohaibAnwaar <sohaibanwaar36@gmail.com>
2021-09-18 20:18:49 +0500

    fix:custom_callback_bug_fix

--- custom_callbacks.py
+++ custom_callbacks.py
@@ -1,11 +1,9 @@
 import keras
 from IPython.display import clear_output
 import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
-import keras
-import numpy as np
+
+
+
 
 '''
 
@@ -78,9 +76,6 @@ class ValidatePredictions(keras.callbacks.Callback):
             
         '''
         
-        # Clear ouput before plotting
-        clear_output(wait=True)
-        
         # Getting the first 5 images
         images_to_pred = self.image[:5]
         
@@ -88,7 +83,6 @@ class ValidatePredictions(keras.callbacks.Callback):
         pred = self.model.predict(images_to_pred)
         
         # Plotting images
-        print("Start epoch {}".format(epoch))
         for i in range(images_to_pred.shape[-1]):
             self.display([self.image[i], self.label[i], pred[i]])
             
@@ -96,47 +90,3 @@ class ValidatePredictions(keras.callbacks.Callback):
             
 
 
-class TrainingPlot(keras.callbacks.Callback):
-
-    # This function is called when the training begins
-    def on_train_begin(self, logs={}):
-        # Initialize the lists for holding the logs, losses and accuracies
-        self.losses = []
-        self.acc = []
-        self.val_losses = []
-        self.val_acc = []
-        self.logs = []
-
-    # This function is called at the end of each epoch
-    def on_epoch_end(self, epoch, logs={}):
-
-        # Append the logs, losses and accuracies to the lists
-        self.logs.append(logs)
-        self.losses.append(logs.get('loss'))
-        self.acc.append(logs.get('acc'))
-        self.val_losses.append(logs.get('val_loss'))
-        self.val_acc.append(logs.get('val_acc'))
-
-        # Before plotting ensure at least 2 epochs have passed
-        if len(self.losses) > 1:
-
-            N = np.arange(0, len(self.losses))
-
-            # You can chose the style of your preference
-            # print(plt.style.available) to see the available options
-            #plt.style.use("seaborn")
-
-            # Plot train loss, train acc, val loss and val acc against epochs passed
-            plt.figure()
-            plt.plot(N, self.losses, label = "train_loss")
-            plt.plot(N, self.acc, label = "train_acc")
-            plt.plot(N, self.val_losses, label = "val_loss")
-            plt.plot(N, self.val_acc, label = "val_acc")
-            plt.title("Training Loss and Accuracy [Epoch {}]".format(epoch))
-            plt.xlabel("Epoch #")
-            plt.ylabel("Loss/Accuracy")
-            plt.legend()
-            # Make sure there exists a folder called output in the current directory
-            # or replace 'output' with whatever direcory you want to put in the plots
-            plt.savefig('output/Epoch-{}.png'.format(epoch))
-            plt.close()
