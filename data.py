d85b89764c3db2a2d4be7090e657bee52b0a4ca4
bose1998 <90802245+bose1998@users.noreply.github.com>
2022-02-12 17:06:26 +0530

    Update data.py
    Updated code based on new skimage package.

--- data.py
+++ data.py
@@ -5,6 +5,7 @@ import os
 import glob
 import skimage.io as io
 import skimage.transform as trans
+from skimage import img_as_ubyte
 
 Sky = [128,128,128]
 Building = [128,0,0]
@@ -121,4 +122,4 @@ def labelVisualize(num_class,color_dict,img):
 def saveResult(save_path,npyfile,flag_multi_class = False,num_class = 2):
     for i,item in enumerate(npyfile):
         img = labelVisualize(num_class,COLOR_DICT,item) if flag_multi_class else item[:,:,0]
-        io.imsave(os.path.join(save_path,"%d_predict.png"%i),img)
+        io.imsave(os.path.join(save_path,"%d_predict.png"%i),img_as_ubyte(img))
