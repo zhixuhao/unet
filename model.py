f79ae19cdf2f9155c9ef6d671d19a0919a3d6140
bose1998 <90802245+bose1998@users.noreply.github.com>
2022-02-12 17:04:33 +0530

    Update model.py
    Updated the code based on updated frameworks

--- model.py
+++ model.py
@@ -5,9 +5,9 @@ import skimage.transform as trans
 import numpy as np
 from keras.models import *
 from keras.layers import *
-from keras.optimizers import *
+from keras.optimizers import adam_v2
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from keras import backend as keras
+
 
 
 def unet(pretrained_weights = None,input_size = (256,256,1)):
@@ -52,9 +52,9 @@ def unet(pretrained_weights = None,input_size = (256,256,1)):
     conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
 
-    model = Model(input = inputs, output = conv10)
+    model = Model(inputs = inputs, outputs = conv10)
 
-    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    model.compile(optimizer = adam_v2.Adam(learning_rate = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
     
     #model.summary()
 
