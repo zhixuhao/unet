15b02820a88668b8565ffcb5dbc8c05845b1fbbf
Musa Akyüz <62879859+MusaAkyuz@users.noreply.github.com>
2023-06-06 12:27:49 +0300

    Add files via upload

--- model.py
+++ model.py
@@ -56,7 +56,7 @@ def unet(pretrained_weights = None,input_size = (256,256,1)):
 
     model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
     
-    #model.summary()
+    model.summary()
 
     if(pretrained_weights):
     	model.load_weights(pretrained_weights)
